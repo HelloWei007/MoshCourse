484a6f64977059acec8070daaa849a3ffa4e8285
HelloWei007 <jiawei.tang1996@gmail.com>
2018-07-29 20:03:48 +0200

    simply post server, express, joi

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const Joi = require('joi');
+
+app.use(express.json());
 
 const courses = [
     {id: 1, name: 'course1'},
@@ -18,7 +21,29 @@ app.get('/api/courses/:id',(req,res)=>{
     const course = courses.find( c => c.id == parseInt(req.params.id));
     if(!course) res.status(404).send('the course not found');
     res.send(course);
+
 }); 
+
+//use postman and send /api/courses with body (json) _> "name" : x
+app.post('/api/courses',(req,res)=>{
+
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    const result = Joi.validate(req.body, schema);
+    
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    console.log(req.body);    
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
 
 
 const port = process.env.PORT ||3000;

c42625616e8a219f211a87d7e5358fb06c2d7520
HelloWei007 <jiawei.tang1996@gmail.com>
2018-07-29 19:24:53 +0200

    simply example node.js get-rest-api with express framework

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-var _ = require('underscore');
+const express = require('express');
+const app = express();
 
-var result = _.contains([1,2,3],2);
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+];
 
-console.log(result);
+app.get('/',(req,res)=>{
+    res.send('hello world');    
+});
+app.get('/api/courses',(req,res)=>{
+    res.send(courses);    
+}); 
+
+app.get('/api/courses/:id',(req,res)=>{
+    const course = courses.find( c => c.id == parseInt(req.params.id));
+    if(!course) res.status(404).send('the course not found');
+    res.send(course);
+}); 
+
+
+const port = process.env.PORT ||3000;
+app.listen(port,()=> console.log(`listening on port ${port}`));

242d8d34e90cbbe65e4e4d93f2fa02b8d5417caf
HelloWei007 <jiawei.tang1996@gmail.com>
2018-07-31 16:19:21 +0200

    example with callback with callback hell problem

--- async-example/index.js
+++ async-example/index.js
@@ -1,13 +1,47 @@
 console.log('Before');
-const user = getUser(1);;
-console.log(user);
+getUser(1,(user) => {
+    getRepositories(user.githubUsername, (repository) => {
+        console.log('repository', repository);
+        getCommits(repository,(commits)=>{
+
+        });
+    });
+});
 console.log('After');
 
+// if you want to get the return value from
+//asynchrono methor you must use 
+// callbacks, Promises , Async/await
 
-function getUser(id){
+
+//It is very important diferent between the Asy and Syn
+//If you want t write the previois code in Syn:
+
+/*console.log('Before');
+const user = getUser(1);
+const repos = getRepositories(user.githubUsername);
+const commits = getCommits(repos[0]);
+console.log('After');*/
+
+function getUser(id, callback) {
     setTimeout(() => {
         console.log('database...');
-    return{id:id, githubUsername:'jiawei'};
+        callback({
+            id: id,
+            githubUsername: 'jiawei'
+        });
+
     }, 2000)
-    return 1;
-}   
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        callback(['repo1', 'repo2', 'repo3']);
+    }, 2000)
+}
+
+function getCommits(repository, callback) {
+    setTimeout(() => {
+        callback("commit");
+    }, 2000)
+}
